48ec333942bc72be21701b2817e5b0bf4dcab109
emilbisak <bisak.emil@gmail.com>
2025-12-04 16:04:16 +0100

    added "X" (close button) for drawer on small screen sizes

--- src/features/products/components/ProductDetailDrawer.tsx
+++ src/features/products/components/ProductDetailDrawer.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+import {Theme, useTheme} from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 import ProductDetailContent from "./ProductDetailContent";
 
@@ -16,13 +20,45 @@ type ProductDetailDrawerProps = {
 const ProductDetailDrawer: React.FC<ProductDetailDrawerProps> = ({id, open, onClose}) => {
     const hasProduct: boolean = typeof id === "number";
 
+    const theme: Theme = useTheme();
+    const isMobile: boolean = useMediaQuery(theme.breakpoints.down("sm"));
+
     return (
         <Drawer
             anchor="right"
             open={open}
             onClose={onClose}
+            slotProps={{
+                paper: {
+                    sx: {
+                        width: {xs: "100vw", sm: 520},
+                    },
+                }
+            }}
         >
-            <Box sx={{width: {xs: "100vw", sm: 520}, p: 3}}>
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    px: 2,
+                    py: 1.5,
+                    borderBottom: "1px solid",
+                    borderColor: "divider",
+                }}
+            >
+                <Typography variant="body1">
+                    Product Details
+                </Typography>
+
+                {isMobile && (
+                    <IconButton onClick={onClose}>
+                        <CloseIcon/>
+                    </IconButton>
+                )}
+            </Box>
+            
+            <Box sx={{p: 3}}>
                 {hasProduct ? (
                     // key ensures that the internal state resets when the id changes
                     <ProductDetailContent
